6285b1a51135b79f4606122bcbdbd8e75df3d437
Fritz Gerneth <fgerneth@effms.de>
2013-04-05 20:49:18 +0200

    Group tests by package and subpackages to be consistent when running all tests

--- tests/Routes/RegexTest.js
+++ tests/Routes/RegexTest.js
@@ -1,7 +1,7 @@
 (function (Tinytest, RegexRoute) {
     "use strict";
 
-    Tinytest.add('RegexRoute: constructor argument must be regex', function (test) {
+    Tinytest.add('ia-route-core - RegexRoute - constructor argument must be regex', function (test) {
         var exceptionCaught = false;
         try {
             new RegexRoute('notARegex');
@@ -11,23 +11,23 @@
         test.isTrue(exceptionCaught);
     });
 
-    Tinytest.add('RegexRoute: doesMatch true if regex matches', function (test) {
+    Tinytest.add('ia-route-core - RegexRoute - doesMatch true if regex matches', function (test) {
         var route = new RegexRoute(/test/g);
         test.isTrue(route.doesMatch('test'));
     });
 
-    Tinytest.add('RegexRoute: doesMatch false if regex does not match', function (test) {
+    Tinytest.add('ia-route-core - RegexRoute - doesMatch false if regex does not match', function (test) {
         var route = new RegexRoute(/test/g);
         test.isFalse(route.doesMatch('notMatching'));
     });
 
-    Tinytest.add('RegexRoute: match returns RouteMatch with matched parameters', function (test) {
+    Tinytest.add('ia-route-core - RegexRoute - match returns RouteMatch with matched parameters', function (test) {
         var route = new RegexRoute(/(\w+)\/(\d)/g),
             result = route.match('name/5');
         test.equal(result.getParams(), {0: 'name', 1: '5'});
     });
 
-    Tinytest.add('RegexRoute: match throws exception if not matching', function (test) {
+    Tinytest.add('ia-route-core - RegexRoute - match throws exception if not matching', function (test) {
         var route = new RegexRoute(/(\d)/g),
             exceptionCaught = false;
         try {
@@ -38,13 +38,13 @@
         test.isTrue(exceptionCaught);
     });
 
-    Tinytest.add('RegexRoute: match applies defaults to RouteMatch', function (test) {
+    Tinytest.add('ia-route-core - RegexRoute - match applies defaults to RouteMatch', function (test) {
         var route = new RegexRoute(/(\/matched)/g, {template: 'test'}),
             result = route.match('/matched');
         test.equal(result.getParam('template'), 'test');
     });
 
-    Tinytest.add('RegexRoute: can re-run regex with g-flag', function (test) {
+    Tinytest.add('ia-route-core - RegexRoute - can re-run regex with g-flag', function (test) {
         var route = new RegexRoute(/(\/matched)/g),
             firstResult = route.match('/matched'),
             // this would throw an exception otherwise
